ee6ec02b95d638f15421abcbd9c4340268d60193
heenasha149 <57365398+heenasha149@users.noreply.github.com>
2020-11-20 21:16:14 +0530

    Update design.js

--- design.js
+++ design.js
@@ -1,6 +1,6 @@
 
 var team1={
-    name:"rcb",
+    name:"RCB",
     score:0,
     runs:[]
 };
@@ -19,11 +19,11 @@ console.log(team2)
 var toss;
 
 window.onload=()=>{
-// console.log("hy")
+
 selectToss(); //selecting strike team
-updateName(); //getting team name dynamically
-updateButtontext(); //
-updateScores();  //
+updateName(); 
+updateButtontext(); 
+updateScores();  
 }
 
 function selectToss(){
@@ -34,21 +34,23 @@ function selectToss(){
 function updateName(){
     console.log("team1 name"+team1.name)
     console.log("team2 name"+team2.name)
+    document.getElementById("team1name").textContent=team1.name;
+    document.getElementById("team2name").textContent=team2.name;
 }
 
 function updateButtontext(){
-    // if(toss===1){
-    //     console.log("team 1 "+team1.name +"batting")
-    // }
-    // else{
-    //     console.log("team 2 "+team2.name +"batting")
-    // }
-//    console.log( `${toss===1?team1.name:team2.name}BATTING`);
-
+    
+var button = document.getElementById("strikebutton")
+var result=document.getElementById("result")
+result.style.visibility=""
    //check if the game is over
    if(team1.runs.length==6 && team2.runs.length==6){
-        console.log("both team finisihed their chances");
-   }
+       button.remove();
+        // console.log("both team finisihed their chances");
+        console.log(team1.score===team2.score?"it is a draw":`${team1.score>team2.score?team1.name:team2.name} Wins`)
+   result.textContent=team1.score===team2.score?"it is a draw":`${team1.score>team2.score?team1.name:team2.name} Wins`
+
+    }
    else{
        //Strike over
        toss=
@@ -57,6 +59,7 @@ function updateButtontext(){
        toss;
    }
    console.log( `${toss===1?team1.name:team2.name}BATTING`)
+   document.getElementById("strikebutton").textContent= `${toss===1?team1.name:team2.name} BATTING`;
 }
 
 handlestrike=()=>{
@@ -80,8 +83,24 @@ handlestrike=()=>{
 function updateScores(){
     console.log("team1score"+team1.score)
     console.log("team2score"+team2.score)
+    document.getElementById("team1score").textContent=team1.score;
+    document.getElementById("team2score").textContent=team2.score;
+    updateRuns();
 }
 
+function updateRuns(){
+    var team1runs = document.getElementById("team1runs").children
+    var team2runs = document.getElementById("team2runs").children;
+
+   team1.runs.forEach((run,index)=>{
+       team1runs[index].textContent=run
+   }); 
+//for(int i =0;i<n;i++)
+
+   team2.runs.forEach((runs,i)=>{
+    team2runs[i].textContent=runs;
+   });
+}
 var calculatescore=(runs)=>{
     return runs.map(num=>{
         return num;
